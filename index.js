57f6fa9194914ebf71b9ea0146fe07cf6eaef316
Bruno Ferreira <brunobrunobrunoferreira@hotmail.com>
2022-10-11 21:45:24 +0100

    add backgroud image to the cavas and resized

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const canvas = document.getElementById("game");
 const context = canvas.getContext("2d");
-
-console.log("bananas");
+const bkImage = new Image();
+bkImage.src = "./Imagens/bk-fish.webp";
+bkImage.onload = () => {
+context.drawImage(bkImage,0,0);
+}
